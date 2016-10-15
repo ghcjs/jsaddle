08ebaa7a5b47149c366527afad7121627879f4a5
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-10-16 11:32:14 +1300

    travis fixes

--- travis/build.sh
+++ travis/build.sh
@@ -4,6 +4,8 @@ echo $PATH
 export LC_ALL=C.UTF-8
 GHCVER=`ghc --numeric-version`
 
+JSADDLE_VERSION=`head -n2 jsaddle.cabal | tail -n1 | sed 's/[^0-9.]//g'`
+
 if [[ -d .cabal && -d .ghc ]]; then
     cp -a .cabal .ghc /root
 fi
@@ -12,7 +14,7 @@ npm install ws
 
 cabal update
 cabal new-build 'jsaddle:lib:jsaddle' 'jsaddle:test:test-tool'
-GHC_PACKAGE_PATH=/opt/ghc/$GHCVER/lib/ghc-$GHCVER/package.conf.d:~/.cabal/store/ghc-$GHCVER/package.db:./dist-newstyle/packagedb/ghc-$GHCVER jsaddle_datadir=`pwd` ./dist-newstyle/build/jsaddle-0.5.0.0/build/test-tool/test-tool
+GHC_PACKAGE_PATH=/opt/ghc/$GHCVER/lib/ghc-$GHCVER/package.conf.d:~/.cabal/store/ghc-$GHCVER/package.db:./dist-newstyle/packagedb/ghc-$GHCVER jsaddle_datadir=`pwd` ./dist-newstyle/build/jsaddle-$JSADDLE_VERSION/build/test-tool/test-tool
 
 # update the cache
 rm -rf .cabal
