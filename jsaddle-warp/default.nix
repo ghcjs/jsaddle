b06fdb0f1cb7f217f3926edd927b39d5f8dadaf0
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-01-21 10:28:34 +0000

    Fix for jsaddle-warp on linux

--- jsaddle-warp/default.nix
+++ jsaddle-warp/default.nix
@@ -2,7 +2,7 @@
 , doctest, filepath, ghc-prim, http-types, jsaddle, lens, primitive
 , process, QuickCheck, ref-tf, stdenv, stm, text, time
 , transformers, wai, wai-websockets, warp, websockets, osx_sdk
-, darwin, webdriver, phantomjs
+, darwin, webdriver, phantomjs, ghc
 }:
 mkDerivation {
   pname = "jsaddle-warp";
@@ -10,9 +10,12 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     aeson base containers http-types jsaddle stm text time transformers
-    wai wai-websockets warp websockets osx_sdk darwin.libobjc
-    darwin.apple_sdk.libs.xpc
-  ];
+    wai wai-websockets warp websockets
+  ] ++ (if ghc.isGhcjs or false then [
+  ] else if stdenv.isDarwin then [
+#    osx_sdk darwin.libobjc darwin.apple_sdk.libs.xpc
+  ] else [
+  ]);
   testHaskellDepends = [
     base bytestring deepseq doctest filepath ghc-prim jsaddle lens
     primitive process QuickCheck ref-tf webdriver phantomjs
