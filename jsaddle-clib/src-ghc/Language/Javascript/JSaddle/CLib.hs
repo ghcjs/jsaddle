56d74735d382cf030ff27dd9d4385959487fbda3
Dan Haraj <dan@obsidian.systems>
2017-02-19 21:48:13 -0500

    make safer

--- jsaddle-clib/src-ghc/Language/Javascript/JSaddle/CLib.hs
+++ jsaddle-clib/src-ghc/Language/Javascript/JSaddle/CLib.hs
@@ -9,11 +9,11 @@ import Control.Concurrent (forkIO)
 
 import Data.Monoid ((<>))
 import Data.ByteString (useAsCString, packCString)
-import Data.ByteString.Unsafe (unsafeUseAsCString)
 import Data.ByteString.Lazy (ByteString, toStrict, fromStrict)
+import Data.ByteString.Char8 (unpack)
 import Data.Aeson (encode, decode)
 
-import Foreign.C.String (CString)
+import Foreign.C.String (CString, newCString)
 import Foreign.Ptr (FunPtr, Ptr)
 import Foreign.Marshal.Utils (new)
 
@@ -23,13 +23,13 @@ import Language.Javascript.JSaddle.Run.Files (initState, runBatch, ghcjsHelpers)
 
 import Language.Javascript.JSaddle.CLib.Internal
 
-foreign import ccall "dynamic"
+foreign import ccall safe "dynamic"
   mkCallback :: FunPtr (CString -> IO ()) -> CString -> IO ()
 
-foreign import ccall "wrapper"
+foreign import ccall safe "wrapper"
   wrapStartCallback :: IO () -> IO (FunPtr (IO ()))
 
-foreign import ccall "wrapper"
+foreign import ccall safe "wrapper"
   wrapMessageCallback :: (CString -> IO ()) -> IO (FunPtr (CString -> IO ()))
 
 jsaddleInit :: JSM () -> FunPtr (CString -> IO ()) -> IO (Ptr NativeCallbacks)
@@ -38,20 +38,20 @@ jsaddleInit jsm evaluateJavascriptAsyncPtr = do
   (processResult, start) <- runJavaScript (\batch ->
     useAsCString (toStrict $ "runJSaddleBatch(" <> encode batch <> ");") $
       evaluateJavascriptAsync) jsm
-  jsaddleStartPtr <- wrapStartCallback $ void $ forkIO start
+  jsaddleStartPtr <- wrapStartCallback $ void $ forkIO $ start
   jsaddleResultPtr <- wrapMessageCallback $ \s -> do
     result <- decode . fromStrict <$> packCString s
     case result of
       Nothing -> error $ "jsaddle message decode failed: " <> show result
       Just r -> processResult r
-  unsafeUseAsCString (toStrict jsaddleJs) $ \jsaddleJsData ->
-    unsafeUseAsCString (toStrict indexHtml) $ \jsaddleHtmlData -> new $
-      NativeCallbacks
-        { _nativeCallbacks_jsaddleStart = jsaddleStartPtr
-        , _nativeCallbacks_jsaddleResult = jsaddleResultPtr
-        , _nativeCallbacks_jsaddleJsData = jsaddleJsData
-        , _nativeCallbacks_jsaddleHtmlData = jsaddleHtmlData
-        }
+  jsaddleJsPtr <- newCString $ unpack $ toStrict jsaddleJs
+  jsaddleHtmlPtr <- newCString $ unpack $ toStrict indexHtml
+  new $ NativeCallbacks
+    { _nativeCallbacks_jsaddleStart = jsaddleStartPtr
+    , _nativeCallbacks_jsaddleResult = jsaddleResultPtr
+    , _nativeCallbacks_jsaddleJsData = jsaddleJsPtr
+    , _nativeCallbacks_jsaddleHtmlData = jsaddleHtmlPtr
+    }
 
 jsaddleJs :: ByteString
 jsaddleJs = ghcjsHelpers <> "\
@@ -60,8 +60,7 @@ jsaddleJs = ghcjsHelpers <> "\
     \ return function(batch) {\n\
     \ " <> runBatch (\a -> "jsaddle.postMessage(JSON.stringify(" <> a <> "));") <> "\
     \ };\n\
-    \})();\n\
-    \\0"
+    \})();\n"
 
 indexHtml :: ByteString
 indexHtml =
@@ -72,4 +71,4 @@ indexHtml =
     \</head>\n\
     \<body>\n\
     \</body>\n\
-    \</html>\0"
+    \</html>"
