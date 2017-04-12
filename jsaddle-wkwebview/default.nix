dbd6394f59d2eea196f78925c8b14043ef82e35d
John Ericson <Ericson2314@Yahoo.com>
2017-04-12 01:04:51 -0400

    jsaddle-wkwebview: Update default.nix for new cross stuff

--- jsaddle-wkwebview/default.nix
+++ jsaddle-wkwebview/default.nix
@@ -1,11 +1,14 @@
-{ mkDerivation, aeson, base, bytestring, jsaddle, stdenv
-, osx_sdk, darwin }:
+{ mkDerivation, stdenv, aeson, base, bytestring, jsaddle
+, buildPackages
+}:
+
 mkDerivation {
   pname = "jsaddle-wkwebview";
   version = "0.8.2.0";
   src = ./.;
-  libraryHaskellDepends = [ aeson base bytestring jsaddle
-      osx_sdk darwin.libobjc darwin.apple_sdk.libs.xpc ];
+  libraryHaskellDepends = [ aeson base bytestring jsaddle ]
+    # should use `librarySystemDepends` but it is not propagated
+    ++ (with buildPackages; [ osx_sdk darwin.libobjc darwin.apple_sdk.libs.xpc ]);
   description = "Interface for JavaScript that works with GHCJS and GHC";
   license = stdenv.lib.licenses.mit;
 }
