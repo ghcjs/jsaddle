5c695f92f3567855948488af8e4344975bfff99b
Ali Abrar <aliabrar@gmail.com>
2017-05-22 16:53:17 -0400

    Fix app_callbacks struct

--- jsaddle-clib/cbits/include/jsaddle.h
+++ jsaddle-clib/cbits/include/jsaddle.h
@@ -9,13 +9,13 @@ typedef struct native_callbacks {
 } native_callbacks;
 
 typedef struct app_callbacks {
-  void (* mainActivity_onCreate) ()
-  void (* mainActivity_onStart) ()
-  void (* mainActivity_onResume) ()
-  void (* mainActivity_onPause) ()
-  void (* mainActivity_onStop) ()
-  void (* mainActivity_onDestroy) ()
-  void (* mainActivity_onRestart) ()
+  void (* mainActivity_onCreate) ();
+  void (* mainActivity_onStart) ();
+  void (* mainActivity_onResume) ();
+  void (* mainActivity_onPause) ();
+  void (* mainActivity_onStop) ();
+  void (* mainActivity_onDestroy) ();
+  void (* mainActivity_onRestart) ();
   void (* firebaseInstanceIdService_sendRegistrationToServer) (char *);
 } app_callbacks;
 
