a9a8c1afbdf22bbf2e0871382632a9a002662af3
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-06-22 21:41:28 -0400

    Use the new libraryDarwinFrameworkDepends

--- jsaddle-wkwebview/default.nix
+++ jsaddle-wkwebview/default.nix
@@ -6,15 +6,14 @@ mkDerivation {
   pname = "jsaddle-wkwebview";
   version = "0.9.0.0";
   src = ./.;
-  libraryHaskellDepends = [ aeson base bytestring jsaddle data-default ]
-    # should use `librarySystemDepends` but it is not propagated
-    ++ (with buildPackages; [
-      darwin.libobjc
-      darwin.apple_sdk.libs.xpc
-      darwin.apple_sdk.frameworks.Foundation
-      darwin.apple_sdk.frameworks.Cocoa
-      darwin.apple_sdk.frameworks.WebKit
-    ]);
+  libraryHaskellDepends = [ aeson base bytestring jsaddle data-default ];
+  libraryDarwinFrameworkDepends = with buildPackages; [
+    darwin.libobjc
+    darwin.apple_sdk.libs.xpc
+    darwin.apple_sdk.frameworks.Foundation
+    darwin.apple_sdk.frameworks.Cocoa
+    darwin.apple_sdk.frameworks.WebKit
+  ];
   description = "Interface for JavaScript that works with GHCJS and GHC";
   license = stdenv.lib.licenses.mit;
 }
