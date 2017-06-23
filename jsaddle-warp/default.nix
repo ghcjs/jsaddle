6bdc4d4ebe742e2005a3d26606e4feb31e8621e2
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-06-22 23:10:19 -0400

    Get rid of an unused import in jsaddle-warp/default.nix

--- jsaddle-warp/default.nix
+++ jsaddle-warp/default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, base, bytestring, containers, deepseq
 , doctest, filepath, ghc-prim, http-types, jsaddle, lens, primitive
 , process, QuickCheck, ref-tf, stdenv, stm, text, time
-, transformers, wai, wai-websockets, warp, websockets, osx_sdk
+, transformers, wai, wai-websockets, warp, websockets
 , darwin, webdriver, phantomjs, ghc
 }:
 mkDerivation {
@@ -12,8 +12,6 @@ mkDerivation {
     aeson base containers http-types jsaddle stm text time transformers
     wai wai-websockets warp websockets
   ] ++ (if ghc.isGhcjs or false then [
-  ] else if stdenv.isDarwin then [
-#    osx_sdk darwin.libobjc darwin.apple_sdk.libs.xpc
   ] else [
   ]);
   testHaskellDepends = [
