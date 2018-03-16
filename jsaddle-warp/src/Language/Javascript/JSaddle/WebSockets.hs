91cbb76bf8d87d9e4f408fe59eacc2fcf0123fa1
Ryan Trinkle <ryan.trinkle@gmail.com>
2018-03-16 19:58:34 -0400

    Support multiple connections again

--- jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
+++ jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
@@ -24,7 +24,7 @@ module Language.Javascript.JSaddle.WebSockets (
   , jsaddleJs
 ) where
 
-import Control.Monad (forever, join)
+import Control.Monad (forever)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Exception (handle, AsyncException, throwIO, fromException)
 
@@ -43,43 +43,49 @@ import Network.Wai.Handler.WebSockets (websocketsOr)
 import Language.Javascript.JSaddle.Types (JSM(..))
 import qualified Network.Wai as W
        (responseLBS, requestMethod, pathInfo)
+import qualified Data.ByteString.Base64.URL as Base64URL
 import qualified Data.Text as T (pack)
+import Data.Text.Encoding (decodeUtf8)
 import qualified Network.HTTP.Types as H
        (status403, status200)
 import Language.Javascript.JSaddle.Run (runJS)
 import Language.Javascript.JSaddle.Run.Files (indexHtml, jsaddleCoreJs, ghcjsHelpers)
 import Data.Maybe (fromMaybe)
 import Data.IORef
-       (readIORef, newIORef, writeIORef)
+       (readIORef, newIORef, atomicModifyIORef')
 import Data.ByteString.Lazy (ByteString)
 import Language.Javascript.JSaddle (SyncCallbackId, ValId)
 import Control.Monad.Trans.Reader
+import qualified Data.Map as Map
+import System.Entropy (getEntropy)
+
+--TODO: stylish-haskell
 
 jsaddleOr :: ConnectionOptions -> JSM () -> Application -> IO Application
 jsaddleOr opts entryPoint otherApp = do
-    --TODO: Allow multiple simultaneous connections
-    processSyncResultRef <- newIORef $ error "processSyncResult not yet set up"
-    let processSyncResult callback this args = do
-          f <- readIORef processSyncResultRef
-          f callback this args
-    continueSyncCallbackRef <- newIORef $ error "continueSyncCallback not yet set up"
-    let continueSyncCallback = do
-          join $ readIORef continueSyncCallbackRef
+    syncFuncs <- newIORef Map.empty
     let wsApp :: ServerApp
         wsApp pending_conn = do
             conn <- acceptRequest pending_conn
-            (processResult, processSyncResult', continueSyncCallback', env) <- runJS (sendTextData conn . encode)
-            writeIORef processSyncResultRef processSyncResult'
-            writeIORef continueSyncCallbackRef continueSyncCallback'
+            (processResult, runSyncCallback, continueSyncCallback, env) <- runJS (sendTextData conn . encode)
+            connId <- decodeUtf8 . Base64URL.encode <$> getEntropy 96
+            let syncFunc = \case
+                    Just (callback, this, args) -> runSyncCallback callback this args
+                    Nothing -> continueSyncCallback
+            sendTextData conn connId
+            atomicModifyIORef' syncFuncs $ \fs ->
+              ( Map.insertWith (error $ "duplicate connection ID" <> show connId) connId syncFunc fs
+              , ()
+              )
             _ <- forkIO . forever $
                 receiveDataMessage conn >>= \case
-                    (WS.Text t) ->
-                        case decode t of
-                            Nothing -> error $ "jsaddle Results decode failed : " <> show t
-                            Just r  -> processResult r
+                    WS.Text t -> case decode t of
+                        Nothing -> error $ "jsaddle response decode failed: " <> show t
+                        Just r  -> processResult r
                     _ -> error "jsaddle WebSocket unexpected binary data"
             runReaderT (unJSM entryPoint) env
             waitTillClosed conn
+            atomicModifyIORef' syncFuncs $ \fs -> (Map.delete connId fs, ())
 
         -- Based on Network.WebSocket.forkPingThread
         waitTillClosed conn = ignore `handle` go 1
@@ -96,15 +102,13 @@ jsaddleOr opts entryPoint otherApp = do
 
         syncHandler :: Application
         syncHandler req sendResponse = case (W.requestMethod req, W.pathInfo req) of
-            ("POST", ["sync"]) -> do
+            ("POST", ["sync", connId]) -> do
+                Just syncFunc <- Map.lookup connId <$> readIORef syncFuncs
                 body <- lazyRequestBody req
                 case decode body :: Maybe (Maybe (SyncCallbackId, ValId, [ValId])) of
-                    Nothing -> error $ "jsaddle sync message decode failed : " <> show body
+                    Nothing -> error $ "jsaddle sync message decode failed: " <> show body
                     Just parsed -> do
-                      --TODO: Combine processSyncResult with continueSyncCallback
-                      result <- case parsed of
-                        Just (callback, this, args) -> processSyncResult callback this args
-                        Nothing -> continueSyncCallback
+                      result <- syncFunc parsed
                       sendResponse $ W.responseLBS H.status200 [("Content-Type", "application/json")] $ encode result
             _ -> otherApp req sendResponse
     return $ websocketsOr opts wsApp syncHandler
@@ -133,6 +137,7 @@ jsaddleAppPartialWithJs js req sendResponse = case (W.requestMethod req, W.pathI
     ("GET", ["jsaddle.js"]) -> Just $ sendResponse $ W.responseLBS H.status200 [("Content-Type", "application/javascript")] js
     _ -> Nothing
 
+--TODO: Make refreshOnLoad work
 -- Use this to generate this string for embedding
 -- sed -e 's|\\|\\\\|g' -e 's|^|    \\|' -e 's|$|\\n\\|' -e 's|"|\\"|g' data/jsaddle.js | pbcopy
 jsaddleJs :: Bool -> ByteString
@@ -146,9 +151,10 @@ jsaddleJs refreshOnLoad = jsaddleCoreJs <> "\
     \    var wsaddress = window.location.protocol.replace('http', 'ws')+\"//\"+window.location.hostname+(window.location.port?(\":\"+window.location.port):\"\");\n\
     \\n\
     \    var ws = new WebSocket(wsaddress);\n\
+    \    var connId;\n\
     \    var sync = function(v) {\n\
     \      var xhr = new XMLHttpRequest();\n\
-    \      xhr.open('POST', '/sync', false);\n\
+    \      xhr.open('POST', '/sync/' + connId, false);\n\
     \      xhr.setRequestHeader(\"Content-type\", \"application/json\");\n\
     \      xhr.send(JSON.stringify(v));\n\
     \      return JSON.parse(xhr.response);\n\
@@ -164,8 +170,11 @@ jsaddleJs refreshOnLoad = jsaddleCoreJs <> "\
     \\n\
     \    ws.onopen = function(e) {\n\
     \\n\
-    \        ws.onmessage = function(e) {\n\
-    \          core.processReq(JSON.parse(e.data));\n\
+    \        ws.onmessage = function(c) {\n\
+    \            connId = c;\n\
+    \            ws.onMessage = function(e) {\n\
+    \                core.processReq(JSON.parse(e.data));\n\
+    \            };\n\
     \        }\n\
     \    };\n\
     \    ws.onerror = function() {\n\
