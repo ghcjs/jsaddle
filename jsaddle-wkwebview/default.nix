b72f3b8bdbfa26fd5ad9e42446b946919f105ed9
Matthew Bauer <mjbauer95@gmail.com>
2018-06-15 17:20:45 -0400

    Fix cflags in jsaddle-wkwebview

--- jsaddle-wkwebview/default.nix
+++ jsaddle-wkwebview/default.nix
@@ -7,6 +7,14 @@ mkDerivation {
   version = "0.9.4.0";
   src = ./.;
   libraryHaskellDepends = [ aeson base bytestring jsaddle data-default ];
+
+  # HACK(matthewbauer): Make sure framework is pulled in first so that
+  # CoreFoundation is correct. Eventually we will update
+  # CoreFoundation in Nixpkgs but that has caused some other issues.
+  preBuild = stdenv.lib.optionalString hostPlatform.useiOSPrebuilt ''
+    NIX_CFLAGS_COMPILE="-F${buildPackages.darwin.xcode_8_2}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks"
+  '';
+
   libraryFrameworkDepends = with buildPackages; if hostPlatform.useiOSPrebuilt then [
     "${darwin.xcode_8_2}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System"
   ] else [
