b6b9cdadd4ffff7fda2d7d956c96afbb78fa350c
John Ericson <John.Ericson@Obsidian.Systems>
2019-10-01 12:59:49 -0400

    Delete Nix
    I believe reflex-platform just uses `callCabal2nix` now; other Nix users
    can do the same.

--- jsaddle-clib/default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, aeson, base, bytestring, data-default, jsaddle, stdenv, text }:
-mkDerivation {
-  pname = "jsaddle-clib";
-  version = "0.9.0.0";
-  src = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [ ".git" "dist" ])) ./.;
-  libraryHaskellDepends = [ aeson base bytestring data-default jsaddle text ];
-  description = "Interface for JavaScript that works with GHCJS and GHC";
-  license = stdenv.lib.licenses.mit;
-}
