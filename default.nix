e671d1851dc335087398951f5ca5fad46093146e
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2020-07-18 17:06:42 +1200

    Add ghc 8.10.1 support and bump version

--- default.nix
+++ default.nix
@@ -1,28 +1,11 @@
-{ pkgs ? import nixpkgs (haskellNixpkgsArgs // (if system == null then {} else { inherit system; }) //
- { overlays = haskellNixpkgsArgs.overlays ++ [(self: super: {
-   inherit (self.haskell-nix.haskellPackages) alex happy;
- })];})
-, nixpkgs ? haskellNixSrc + "/nixpkgs"
-, haskellNixpkgsArgs ? import haskellNixSrc
-, haskellNixSrc ? builtins.fetchTarball {
-    url = "https://github.com/input-output-hk/haskell.nix/archive/1dd6951d6a56115fd368c5b37b8dab47a90ec45c.tar.gz";
-    sha256 = "02nhjy90xwg9wrp81cnlczndxags966c71wlbz6n91423a13mc99";
-  }
-, haskellCompiler ? "ghc881"
+{ sourcesOverride ? {}
+, sources ? import ./nix/sources.nix {} // sourcesOverride
+, pkgs ? (import sources."haskell.nix" {}).pkgs
+, compiler-nix-name ? "ghc8101"
 , system ? null
 }:
-let
-  frameworks = pkgs.lib.optionals pkgs.stdenv.isDarwin (
-    with pkgs.darwin.apple_sdk.frameworks; [ Cocoa Carbon CoreGraphics ]);
-  project = pkgs.haskell-nix.cabalProject' {
-    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-    ghc = pkgs.buildPackages.pkgs.haskell-nix.compiler.${haskellCompiler};
-    modules = [
-      ({ config, ...}: {
-        packages.gi-gtk.components.setup.frameworks = frameworks;
-        packages.vault.components.library.doHaddock = false;
-      })
-    ];
-  };
-in
-  project
+  pkgs.haskell-nix.project {
+    inherit compiler-nix-name;
+    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; name = "jsaddle"; };
+    modules = [{ reinstallableLibGhc = true; packages.jsaddle-warp.components.tests.test-tool.buildable = pkgs.lib.mkForce false; }];
+  }
--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
-{ haskellCompiler ? "ghc881" }:
-(import ./. { inherit haskellCompiler; }).hsPkgs.shellFor {}
-    
+{ sourcesOverride ? {}, compiler-nix-name ? "ghc8101" }:
+(import ./. { inherit sourcesOverride compiler-nix-name; }).shellFor {
+  tools = { cabal = "3.2.0.0"; };
+}
