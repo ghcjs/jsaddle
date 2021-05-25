97d080cda5eb293f629fe4804c9eada03e65fa6d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-05-25 15:50:55 +1200

    Update hix

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,8 +1,8 @@
+{pkgs, ...}:
 { modules = [({pkgs, ...}: {
-    reinstallableLibGhc = !pkgs.stdenv.hostPlatform.isGhcjs;
-    packages.jsaddle-warp.components.tests.test-tool.buildable = pkgs.lib.mkForce false;
     packages.jsaddle-webkit2gtk.components.library.buildable = pkgs.lib.mkForce pkgs.stdenv.hostPlatform.isLinux;
   })];
+  shell.buildInputs = [ pkgs.nodejs ];
   shell.tools.cabal = {};
   shell.crossPlatforms = p: [ p.ghcjs ];
 }
