219bde629b2ee949315fd0ae0430b1e7c5084440
George Thomas <georgefsthomas@gmail.com>
2024-10-23 15:34:00 +0100

    Fix Haddock escaping

--- jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
+++ jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
@@ -244,7 +244,7 @@ jsaddleJs' jsaddleUri refreshOnLoad = "\
 
 -- | Start or restart the server.
 -- To run this as part of every :reload use
--- > :def! reload (const $ return "::reload\nLanguage.Javascript.JSaddle.Warp.debug 3708 SomeMainModule.someMainFunction")
+-- > :def! reload (const $ return "::reload\\nLanguage.Javascript.JSaddle.Warp.debug 3708 SomeMainModule.someMainFunction")
 debug :: Int -> JSM () -> IO ()
 debug port f = do
     debugWrapper $ \withRefresh registerContext ->
